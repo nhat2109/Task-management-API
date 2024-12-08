9062220e773d16ac80ddebdf6c3e905ae00fe02f
nhat2109 <dangnhat20031010@gmail.com>
2024-12-08 13:30:22 +0700

    change token modle -> live in register controller and otp, reset detail

--- API/V1/controllers/user.controller.js
+++ API/V1/controllers/user.controller.js
@@ -19,7 +19,8 @@ module.exports.register = async (req, res) =>{
         const user = new User({
             fullName: req.body.fullName,
             email: req.body.email,
-            password: req.body.password
+            password: req.body.password, 
+            token: generateHelper.generateRandomString(20)
         });
         user.save();
         const token = user.token;
@@ -82,6 +83,7 @@ module.exports.forgotPassword = async (req, res) =>{
         });
         return;
     }
+    // save data to database
     const otp = generateHelper.generateRandomNumber(8);
     const timeExprise = 5;
     const objectForgotPassword ={
@@ -91,15 +93,88 @@ module.exports.forgotPassword = async (req, res) =>{
     };
     const forgotPassword = new ForgotPassword(objectForgotPassword);
     await forgotPassword.save();
+    // sent OPT to email user
     // console.log(objectForgotPassword);
     const subject  = "Code OTP confirm get password";
     const html = `Code OTP confirm get password of your account ${otp} (Use in ${timeExprise} minute to get your password)
     without share password with everyone
     `;
     sendMailHelper.sendMail(email, subject, html);
-    // sent OPT to email user
+    
     res.json({
         code: 200,
         message: "Sent OTP email"
+    });
+}
+
+// [POST] /api/v1/user/password/otp
+module.exports.otpPassword = async (req, res) => {
+    const email = req.body.email;
+    const otp = req.body.otp;
+    const result = await ForgotPassword.findOne({
+        email: email,
+        otp: otp,    
+    });
+    if(!result){
+        res.json({
+            code: 400,
+            message: "OTP fail"
+        });
+        return;
+    }
+    const user = await User.findOne({
+        email: email
+    });
+    const token = user.token;
+    res.cookie("token", token);
+
+    res.json({
+        code: 200,
+        message: "OTP success",
+        token: token
+    });
+}
+
+// [POST] /api/v1/user/password/reset
+module.exports.resetPassword = async (req, res) => {
+    const password = req.body.password;
+    // get token equal 
+    // const token = req.body.token;
+    // get token with cookie
+    const token = req.cookies.token;
+    const user = await User.findOne({
+        token: token,
+    });
+    if(md5(password) === user.password) {
+        res.json({
+            code: 400,
+            message: "Please enter new password different form old password"
+        });
+        return;
+    }
+    await User.updateOne({
+        token: token,
+    }, {
+        password: md5(req.body.password),
+    });
+    res.json({
+        code: 200,
+        message: "Change password was successful"
+    });
+}
+
+// [GET] /api/v1/user/detail
+module.exports.detail = async (req, res) => {
+    const token = req.cookies.token;
+    console.log(token);
+    const user = await User.findOne({
+        token: token,
+        deleted: false
+    }).select("-password -token");
+    res.json({
+        code: 200,
+        message: "Was successful",
+        info: user
+
     });
 }
